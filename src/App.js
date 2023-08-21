ddcf5ef1d1c0c9f20c6ad39c6a579516f74a245b
Abhilash Nair <abhilashnair1994@gmail.com>
2023-08-20 18:42:43 -0600

    learning useState hook

--- src/App.js
+++ src/App.js
@@ -1,38 +1,66 @@
 import './App.css'
 import { Job } from './components/Job'
 import { User } from './components/User'
-import { People, Planets } from './FakeData'
 import { Planet } from './components/Planet'
+import { People, Planets } from './FakeData'
+import { useState } from 'react'
+
 // if renamed to App.modules.css you can specify css objects by using
 // import styles from "./App.module.css" and pass as style={styles.classname}
 
 function App() {
+  const [age, setAge] = useState(0)
+  const [inputValue, setInputValue] = useState('')
+  const [showText, setShowText] = useState(false)
+  const [textColor, setTextColor] = useState('green')
+  const increaseAge = () => {
+    console.log('before', age)
+    setAge(age + 1)
+  }
   return (
     <div className="App">
-      {People.map((person, key) => {
-        return (
-          <div key={key}>
-            <User name={person.name} age={person.age} email={person.email} />
-            <Job
-              salary={person.salary}
-              position={person.position}
-              company={person.company}
-            />
-          </div>
-        )
-      })}
-
-      {Planets.map((planet, key) => {
-        return (
-          <>
-            {planet.isGasPlanet ? (
-              <Planet name={planet.name} />
-            ) : (
-              <h1>Not Gassy enough</h1>
-            )}
-          </>
-        )
-      })}
+      {age}
+      <button
+        onClick={() => {
+          increaseAge()
+        }}
+      >
+        Increase Age
+      </button>
+      <button
+        onClick={() => {
+          setAge(age - 1)
+        }}
+      >
+        Decrease Age
+      </button>
+      <button
+        onClick={() => {
+          setAge(0)
+        }}
+      >
+        0 Age
+      </button>
+      {'  '}
+      <input
+        type="text"
+        placeholder="pass anything..."
+        value={inputValue}
+        onChange={(event) => {
+          console.log(event.target.value)
+          setInputValue(event.target.value)
+        }}
+      />
+      <hr></hr>
+      {showText === true && <h1 style={{ color: textColor }}> Show Text</h1>}
+      <button
+        onClick={() => {
+          setShowText(!showText)
+          setTextColor(textColor === 'green' ? 'red' : 'black')
+        }}
+      >
+        Show/Hide
+      </button>
     </div>
   )
 }
